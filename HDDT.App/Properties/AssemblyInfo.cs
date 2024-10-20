1bfacd73535b3c30f206e1c90d059f90a7977e36
Phúc Phạm <hhtb.bdh@gmail.com>
2024-10-20 16:45:30 +0700

    x

--- HDDT.App/Properties/AssemblyInfo.cs
+++ HDDT.App/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.13.1")]
-[assembly: AssemblyFileVersion("1.0.13.1")]
+[assembly: AssemblyVersion("1.0.14.1")]
+[assembly: AssemblyFileVersion("1.0.14.1")]
