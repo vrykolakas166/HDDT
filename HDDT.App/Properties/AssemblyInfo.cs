fe5801970620a257c04becb334d15faec3651a15
Phúc Phạm <hhtb.bdh@gmail.com>
2024-10-22 16:28:51 +0700

    fix

--- HDDT.App/Properties/AssemblyInfo.cs
+++ HDDT.App/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.70.2")]
-[assembly: AssemblyFileVersion("1.0.70.2")]
+[assembly: AssemblyVersion("1.0.71.2")]
+[assembly: AssemblyFileVersion("1.0.71.2")]
